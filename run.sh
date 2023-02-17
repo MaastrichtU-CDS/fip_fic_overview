b8ffafca92bde0a8e1352bfff96c2fe3899e1cb9
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-02-17 16:42:09 +0100

    Added execution and readme.md

---
+++ run.sh
@@ -0,0 +1,5 @@
+echo "# Overview of FIP questions" > README.md
+echo "This repository contains a generated overview of the FIP questions I2 and I3." >> README.md
+echo "Please check [run.sh](run.sh) for the generation of the README.md file." >> README.md
+echo "" >> README.md
+echo "$(python run.py)" >> README.md
