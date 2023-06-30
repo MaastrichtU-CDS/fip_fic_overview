25d786d25e61ec6f579ed2b9a975d72b7ae65e84
matthijssloep <m.sloep@gmail.com>
2023-06-30 10:35:32 +0200

    test new run2.py

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-echo "# Overview of FIP questions" > README.md
-echo "This repository contains a generated overview of the FIP questions I2 and I3." >> README.md
-echo "Please check [run.sh](run.sh) for the generation of the README.md file." >> README.md
+echo "# Overview of FAIR implementation communities and profiles" > README.md
+echo "This repository gives a daily update of available FAIR implementation communities, profiles and the respective answers to I2 data and I3 metadata." >> README.md
 echo "" >> README.md
-echo "$(python run.py)" >> README.md
+echo "$(python run2.py)" >> README.md
