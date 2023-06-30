f8ff0d3811e634303da6e809cd6e2f0980c29e18
matthijssloep <49636857+matthijssloep@users.noreply.github.com>
2023-06-30 11:50:02 +0200

    Update run.sh testing run.py

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "# Overview of FAIR implementation communities and profiles" > README.md
 echo "This repository gives a daily update of available FAIR implementation communities, profiles and the respective answers to I2 data and I3 metadata." >> README.md
 echo "" >> README.md
-echo "$(python run2.py)" >> README.md
+echo "$(python run.py)" >> README.md
